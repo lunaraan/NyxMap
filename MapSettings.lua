169c36eb1e58892b78d68f5aabd126ac8034f0b2
Chris Opitz <141081747+lunaraan@users.noreply.github.com>
2025-06-26 16:51:02 -0400

    Delete MapSettings.lua

--- MapSettings.lua
+++
@@ -1,8 +0,0 @@
-local MapSettings = {
-    UseDeepEqDefault = false,
-	LenReturnsRootCount = false,
-	AllowAllCustomization = false -- Not recommended. Only for debugging. Setting this to true can result in undefined behavior
-}
-table.freeze(MapSettings)
-
-return MapSettings
